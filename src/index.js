af100e87d07422a35c865b5ab772845b4482f10b
daengdaengLee <gunho1020@gmail.com>
2018-11-05 08:43:53 +0900

    ENH: Setting global style
    - style reset using styled-reset
    - default height 100%

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<div />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  html, body, #root {
+    height: 100%;
+  }
+`;
+
+ReactDOM.render(
+  <Fragment>
+    <div>Hello World</div>
+    <GlobalStyle />
+  </Fragment>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
