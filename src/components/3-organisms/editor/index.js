0840f50075b08e0f5ddb9be95cd272bdee5efba5
daengdaengLee <gunho1020@gmail.com>
2018-11-05 13:36:44 +0900

    ENH: Make user interaction in editor comp
    - move node item
    - connect node items

--- src/components/3-organisms/editor/index.js
+++ src/components/3-organisms/editor/index.js
@@ -1,4 +1,26 @@
 import React, { Component } from 'react';
+import NodeItem from '../../2-molecules/node-item';
+
+const _calcPath = ({
+  fromNodeX,
+  fromNodeY,
+  fromOutCount,
+  fromOutIdx,
+  toNodeX,
+  toNodeY,
+  toInCount,
+  toInIdx,
+}) => {
+  const fromX = fromNodeX + (140 / (fromOutCount + 1)) * (fromOutIdx + 1);
+  const fromY = fromNodeY + 60;
+  const toX = toNodeX + (140 / (toInCount + 1)) * (toInIdx + 1);
+  const toY = toNodeY;
+  return fromY < toY
+    ? `M ${fromX} ${fromY} C ${fromX} ${toY}, ${toX} ${fromY}, ${toX} ${toY}`
+    : `M ${fromX} ${fromY} C ${(fromX + toX) / 2} ${fromY * 1.5 -
+        toY * 0.5}, ${(fromX + toX) / 2} ${toY * 1.5 -
+        fromY * 0.5}, ${toX} ${toY}`;
+};
 
 class Editor extends Component {
   constructor(props) {
@@ -6,9 +28,21 @@ class Editor extends Component {
     this.state = {
       allNodeIds: [],
       nodesById: {},
+      movingId: null,
+      connectNodeId: null,
+      connectOutIdx: null,
     };
     this._container = React.createRef();
     this._onDrop = this._onDrop.bind(this);
+    this._movingStart = this._movingStart.bind(this);
+    this._moving = this._moving.bind(this);
+    this._movingEnd = this._movingEnd.bind(this);
+    this._connectStart = this._connectStart.bind(this);
+    this._connect = this._connect.bind(this);
+    this._connectEnd = this._connectEnd.bind(this);
+    this._onMouseMoveSvg = this._onMouseMoveSvg.bind(this);
+    this._onMouseUpSvg = this._onMouseUpSvg.bind(this);
+    this._onMouseLeaveSvg = this._onMouseLeaveSvg.bind(this);
   }
 
   render() {
@@ -16,8 +50,41 @@ class Editor extends Component {
       _container,
       _onDragOver,
       _onDrop,
+      _movingStart,
+      _connectStart,
+      _connect,
+      _onMouseMoveSvg,
+      _onMouseUpSvg,
+      _onMouseLeaveSvg,
       state: { allNodeIds, nodesById },
     } = this;
+    const connects = allNodeIds
+      .map(id => {
+        const node = nodesById[id];
+        return node.connects.map(connect => {
+          const toNode = node;
+          const fromNode = nodesById[connect.fromNodeId];
+          const fromNodeX = fromNode.x;
+          const fromNodeY = fromNode.y;
+          const fromOutCount = fromNode.outCount;
+          const toNodeX = toNode.x;
+          const toNodeY = toNode.y;
+          const toInCount = toNode.inCount;
+          return {
+            ...connect,
+            fromNodeX,
+            fromNodeY,
+            fromOutCount,
+            toNodeX,
+            toNodeY,
+            toInCount,
+          };
+        });
+      })
+      .reduce(
+        (allConnects, nodeConnects) => [...allConnects, ...nodeConnects],
+        [],
+      );
     return (
       <div
         ref={_container}
@@ -25,11 +92,36 @@ class Editor extends Component {
         onDragOver={_onDragOver}
         onDrop={_onDrop}
       >
-        <svg style={{ width: 0, flexGrow: 1 }}>
+        <svg
+          style={{ width: 0, flexGrow: 1 }}
+          onMouseLeave={_onMouseLeaveSvg}
+          onMouseUp={_onMouseUpSvg}
+          onMouseMove={_onMouseMoveSvg}
+        >
+          {connects.map(connect => (
+            <path
+              key={`${connect.fromNodeId},${connect.fromOutIdx},${
+                connect.toNodeId
+              },${connect.toInIdx}`}
+              d={_calcPath(connect)}
+              stroke="black"
+              fill="none"
+            />
+          ))}
           {allNodeIds.map(id => {
             const node = nodesById[id];
             return (
-              <rect key={id} x={node.x} y={node.y} width="140" height="60" />
+              <NodeItem
+                key={id}
+                id={id}
+                x={node.x}
+                y={node.y}
+                inCount={node.inCount}
+                outCount={node.outCount}
+                onMouseDownNode={_movingStart}
+                onMouseDownOut={_connectStart}
+                onMouseUpIn={_connect}
+              />
             );
           })}
         </svg>
@@ -49,7 +141,7 @@ class Editor extends Component {
     const {
       _container: { current: containerEl },
     } = this;
-    if (!containerEl || inoutCount.length != 2) return;
+    if (!containerEl || inoutCount.length !== 2) return;
     const { offsetLeft, offsetTop } = containerEl;
     const x = clientX - offsetLeft;
     const y = clientY - offsetTop;
@@ -65,11 +157,92 @@ class Editor extends Component {
       y,
       inCount,
       outCount,
+      connects: [],
     };
     this.setState(({ allNodeIds, nodesById }) => ({
       allNodeIds: [...allNodeIds, id],
       nodesById: { ...nodesById, [id]: node },
     }));
+    console.log('[NODE] make');
+  }
+
+  _movingStart(id) {
+    this.setState({ movingId: id });
+  }
+
+  _moving(movementX, movementY) {
+    const {
+      state: { movingId, nodesById },
+    } = this;
+    if (!movingId) return;
+    const node = nodesById[movingId];
+    this.setState(({ nodesById }) => ({
+      nodesById: {
+        ...nodesById,
+        [movingId]: { ...node, x: node.x + movementX, y: node.y + movementY },
+      },
+    }));
+  }
+
+  _movingEnd() {
+    this.setState({ movingId: null });
+  }
+
+  _connectStart(nodeId, outIdx) {
+    this.setState({ connectNodeId: nodeId, connectOutIdx: outIdx });
+  }
+
+  _connect(toNodeId, inIdx) {
+    const {
+      state: { connectNodeId: fromNodeId, connectOutIdx: outIdx, nodesById },
+    } = this;
+    if (!fromNodeId || outIdx == null) return;
+    const connect = {
+      fromNodeId,
+      fromOutIdx: outIdx,
+      toNodeId,
+      toInIdx: inIdx,
+    };
+    const node = nodesById[toNodeId];
+    const isDuplicateConnect = node.connects.find(
+      obj =>
+        obj.fromNodeId === connect.fromNodeId &&
+        obj.fromOutIdx === connect.fromOutIdx &&
+        obj.toNodeId === connect.toNodeId &&
+        obj.toInIdx === connect.toInIdx,
+    );
+    if (isDuplicateConnect) return;
+    this.setState(({ nodesById }) => ({
+      nodesById: {
+        ...nodesById,
+        [toNodeId]: {
+          ...node,
+          connects: [...node.connects, connect],
+        },
+      },
+    }));
+    console.log('[NODE] connect');
+  }
+
+  _connectEnd() {
+    this.setState({ connectNodeId: null, connectOutIdx: null });
+  }
+
+  _onMouseMoveSvg({ movementX, movementY }) {
+    const { _moving } = this;
+    _moving(movementX, movementY);
+  }
+
+  _onMouseUpSvg() {
+    const { _movingEnd, _connectEnd } = this;
+    _movingEnd();
+    _connectEnd();
+  }
+
+  _onMouseLeaveSvg() {
+    const { _movingEnd, _connectEnd } = this;
+    _movingEnd();
+    _connectEnd();
   }
 }
 
